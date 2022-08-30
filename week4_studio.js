36db6a9d13dde0b6eb1f6584b91917ea6aa0ca70
Source Academy User <No public email provided>
2022-08-30 14:29:08 +0800

    Changes made from Source Academy

--- week4_studio.js
+++ week4_studio.js
@@ -1,9 +1,5 @@
 function pascal(row, position) {
-    return row === 0
-           ? 1
-           : position === 0
-           ? 1
-           : position === row
+    return row === 0 || position === 0 || position === row
            ? 1
            : pascal(row - 1, position - 1) + pascal(row - 1, position);
 }
